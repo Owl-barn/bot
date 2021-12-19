63a2e24e96a1fffdfaa36d138b86d6b51d2ba289
valivia <valivia@xayania.com>
2021-12-19 23:59:24 +0000

    get next date function

--- src/lib/functions.service.ts
+++ src/lib/functions.service.ts
@@ -3,3 +3,12 @@ import { GuildMember } from "discord.js";
 export function isDJ(member: GuildMember): boolean {
     return member?.roles.cache.some(role => role.name === "DJ") || member.permissions.has("ADMINISTRATOR") || member.id == process.env.OWNER_ID;
 }
+
+export function nextDate(currentDate: Date): Date {
+    const year = new Date().getUTCFullYear();
+    const nextBirthday = currentDate;
+    nextBirthday.setUTCFullYear(year);
+    if (Number(nextBirthday) < Number(new Date())) nextBirthday.setUTCFullYear(year + 1);
+
+    return nextBirthday;
+}
