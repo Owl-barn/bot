e6920f9ae72696a15f82fe706a488a5a24683487
valivia <valivia@xayania.com>
2021-12-22 23:30:35 +0000

    birthday difference command

--- src/commands/general/birthday.ts
+++ src/commands/general/birthday.ts
@@ -21,30 +21,60 @@ module.exports = class extends Command {
                     name: "get",
                     description: "get an user's birthday",
                     required: false,
-                    subCommand: {
-                        type: argumentType.user,
-                        name: "user",
-                        description: "Who's birthday to get",
-                        required: true,
-                    },
+                    subCommands: [
+                        {
+                            type: argumentType.user,
+                            name: "user",
+                            description: "Who's birthday to get",
+                            required: true,
+                        },
+                    ],
                 },
                 {
                     type: argumentType.subCommand,
                     name: "set",
                     description: "set your birthday",
                     required: false,
-                    subCommand: {
-                        type: argumentType.string,
-                        name: "birthday",
-                        description: "your birthday date formatted like: dd/mm/yyyy",
-                        required: true,
-                    },
+                    subCommands: [
+                        {
+                            type: argumentType.string,
+                            name: "birthday",
+                            description: "your birthday date formatted like: dd/mm/yyyy",
+                            required: true,
+                        },
+                    ],
+                },
+                {
+                    type: argumentType.subCommand,
+                    name: "difference",
+                    description: "get an user's birthday",
+                    required: false,
+                    subCommands: [
+                        {
+                            type: argumentType.user,
+                            name: "user",
+                            description: "Who's birthday to compare",
+                            required: true,
+                        },
+                        {
+                            type: argumentType.user,
+                            name: "second_user",
+                            description: "Who's birthday to compare",
+                            required: true,
+                        },
+                    ],
+                },
+                {
+                    type: argumentType.subCommand,
+                    name: "sync",
+                    description: "fetch birthday from other server",
+                    required: false,
                 },
             ],
 
             throttling: {
-                duration: 720,
-                usages: 2,
+                duration: 120,
+                usages: 3,
             },
         });
     }
@@ -55,6 +85,7 @@ module.exports = class extends Command {
 
         const birthday = msg.options.getString("birthday");
         const user = msg.options.getMember("user") as GuildMember;
+        const second_user = msg.options.getMember("second_user") as GuildMember;
         const subCommand = msg.options.getSubcommand(true);
         const birthdayCheck = new RegExp(/^[0-9]{2}[/]{1}[0-9]{2}[/]{1}[0-9]{4}$/g);
 
@@ -133,6 +164,44 @@ module.exports = class extends Command {
                 .setTimestamp();
 
             return { embeds: [embed] };
+        } else if (subCommand === "difference") {
+            const users = await client.db.birthdays.findMany({
+                where: {
+                    OR: [
+                        { user_id: user.id },
+                        { user_id: second_user.id },
+                    ],
+                    guild_id: msg.guildId,
+                },
+                orderBy: { birthday: "asc" },
+            });
+
+            if (users.length !== 2) return { content: "one or more of you dont have a birthday registered" };
+
+            const difference = Math.abs((Number(users[0].birthday) - Number(users[1].birthday)) / (1000 * 60 * 60 * 24));
+            const years = Math.floor(difference / 365);
+            const days = difference % 365;
+
+            const differenceString = `${years ? `${years} year${years == 1 ? "" : "s"} and` : ""} ${days} day${days == 1 ? "" : "s"}`;
+            const oldest = users[0].user_id === second_user.id ? second_user : user;
+            const youngest = users[1].user_id === second_user.id ? second_user : user;
+
+            const embed = new MessageEmbed()
+                .addField(`Who is older?`, `${oldest} is older than ${youngest} by ${differenceString} ${years > 2 ? `(sussy)` : ""}`)
+                .setFooter(`${msg.user.username} <@${msg.user.id}>`, msg.user.displayAvatarURL())
+                .setColor("RED")
+                .setTimestamp();
+
+            return { embeds: [embed] };
+        } else if (subCommand === "sync") {
+            const query = await client.db.birthdays.findFirst({ where: { user_id: msg.user.id } });
+
+            if (!query) return { content: "You dont have a birthday registered anywhere else." };
+            const result = await client.db.birthdays.create({ data: { user_id: query.user_id, guild_id: msg.guildId, birthday: query.birthday } }).catch(() => null);
+
+            if (!result) return { content: "You already have a birthday registered here" };
+
+            return { content: "birthday has been transferred!" };
         }
 
         throw "no subcommand!??";
