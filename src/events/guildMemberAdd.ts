c8a64ac0652f813c22dcbf086d2b93d7be83205c
valivia <valivia@xayania.com>
2022-02-06 16:17:04 +0000

    add level rewards on rejoin

---
+++ src/events/guildMemberAdd.ts
@@ -0,0 +1,38 @@
+import { GuildMember, RoleResolvable } from "discord.js";
+import levelService from "../lib/level.service";
+import RavenEvent from "../types/event";
+import RavenClient from "../types/ravenClient";
+
+export default class implements RavenEvent {
+    name = "guildMemberAdd";
+    once = false;
+
+    async execute(member: GuildMember): Promise<void> {
+        const client = member.client as RavenClient;
+
+        const userLevel = await client.db.level.findUnique({
+            where: {
+                user_id_guild_id: {
+                    user_id: member.id,
+                    guild_id: member.guild.id,
+                },
+            },
+        });
+
+        if (!userLevel) return;
+
+        const level = levelService.calculateLevel(userLevel.experience);
+        const rewards = await client.db.level_reward.findMany({ where: { level: { lte: level.level }, guild_id: member.guild.id } });
+
+        if (rewards.length === 0) return;
+
+        const roles: RoleResolvable[] = [];
+        for (const x of rewards) {
+            const role = member.guild.roles.resolveId(x.role_id);
+            if (!role) continue;
+            roles.push(role);
+        }
+
+        await member.roles.add(roles, "Level roles").catch(() => console.log("Couldnt assign roles"));
+    }
+}
