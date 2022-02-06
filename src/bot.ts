b93451c148d3d4fef273c7ccc57c6b17482d31cc
valivia <valivia@xayania.com>
2022-02-06 16:17:15 +0000

    added intent

--- src/bot.ts
+++ src/bot.ts
@@ -16,6 +16,7 @@ export default class Bot {
                 Intents.FLAGS.GUILDS,
                 Intents.FLAGS.GUILD_MESSAGES,
                 Intents.FLAGS.GUILD_VOICE_STATES,
+                Intents.FLAGS.GUILD_MEMBERS,
             ], allowedMentions: { parse: ["users"] },
         });
 
