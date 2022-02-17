c451a8ad98351500830ceaab104256ebfbb779a3
valivia <valivia@xayania.com>
2022-02-17 20:02:49 +0000

    reset all levels command

---
+++ src/commands/moderation/config/level/reset.module.ts
@@ -0,0 +1,15 @@
+import { HexColorString, MessageEmbed } from "discord.js";
+import { returnMessage } from "../../../../types/Command";
+import RavenInteraction from "../../../../types/interaction";
+
+export async function configLevelReset(msg: RavenInteraction): Promise<returnMessage> {
+    if (!msg.guildId) throw "no guild??";
+
+    const deleted = await msg.client.db.level.deleteMany({ where: { guild_id: msg.guildId } });
+
+    const embed = new MessageEmbed()
+        .setDescription(`Successfully deleted \`${deleted.count}\` user levels`)
+        .setColor(process.env.EMBED_COLOR as HexColorString);
+
+    return { embeds: [embed] };
+}
