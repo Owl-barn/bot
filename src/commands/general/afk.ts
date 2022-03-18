7e9a50f902811d30b023f916f8c6d5e4483fe957
valivia <valivia@xayania.com>
2022-03-18 15:45:28 +0000

    afk command

---
+++ src/commands/general/afk.ts
@@ -0,0 +1,68 @@
+import { HexColorString, MessageEmbed } from "discord.js";
+import AFKService from "../../lib/afk.service";
+import { argumentType } from "../../types/argument";
+import { Command, returnMessage } from "../../types/Command";
+import { CommandGroup } from "../../types/commandGroup";
+import RavenInteraction from "../../types/interaction";
+
+module.exports = class extends Command {
+    constructor() {
+        super({
+            name: "afk",
+            description: "tells people you are AFK",
+            group: CommandGroup.general,
+
+            guildOnly: true,
+
+            args: [
+                {
+                    type: argumentType.string,
+                    name: "reason",
+                    description: "Why are you going AFK?",
+                    required: false,
+                },
+                {
+                    type: argumentType.boolean,
+                    name: "global",
+                    description: "Go AFK everywhere or just here? (defaults to true)",
+                    required: false,
+                },
+            ],
+
+            throttling: {
+                duration: 60,
+                usages: 1,
+            },
+        });
+    }
+
+    async execute(msg: RavenInteraction): Promise<returnMessage> {
+        const client = msg.client;
+        let global = msg.options.getBoolean("global");
+        let reason = msg.options.getString("reason");
+
+        reason = reason ? reason.substring(0, 127) : null;
+        global = global === null ? true : global;
+
+        await client.db.afk.deleteMany({ where: { user_id: msg.user.id, global: true } });
+
+        const createQuery = await client.db.afk.create({
+            data: {
+                user_id: msg.user.id,
+                guild_id: msg.guildId || "000000000000000000",
+                reason,
+                global,
+            },
+        });
+
+        AFKService.setAFK(createQuery);
+
+
+        const embed = new MessageEmbed()
+            .setTitle(`AFK set`)
+            .setDescription(`Successfully set you as AFK${reason ? ` message: \`\`${reason}\`\`` : ""} `)
+            .setColor(process.env.EMBED_COLOR as HexColorString);
+
+        return { embeds: [embed] };
+    }
+};
--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,5 +1,8 @@
 import { self_role_main } from "@prisma/client";
 import { Guild, GuildChannel, HexColorString, Message, MessageActionRow, MessageActionRowComponent, MessageButton, MessageEmbed, TextChannel } from "discord.js";
+import AFKService from "../lib/afk.service";
+import prisma from "../lib/db.service";
+import { yearsAgo } from "../lib/functions.service";
 import levelService from "../lib/level.service";
 import registerCommand, { registerPerms } from "../modules/command.register";
 import RavenEvent from "../types/event";
@@ -13,7 +16,13 @@ export default class InteractionCreate implements RavenEvent {
         if (!msg) return;
         if (msg.author.bot) return;
         levelService.message(msg).catch((x) => console.error(x));
+        AFKService.onMessage(msg).catch((x) => console.error(x));
         const client = msg.client as RavenClient;
+
+        if (msg.content === "-cookie" && msg.guildId === "396330910162616321") {
+            msg.reply("$suppressErrors").catch((x) => console.log(x));
+            return;
+        }
 
         if (msg.content === "update*" && msg.member?.id === process.env.OWNER_ID) {
             const guilds = client.guilds.cache;
@@ -46,6 +55,51 @@ export default class InteractionCreate implements RavenEvent {
             await channel.send(msg.content.substring(5, msg.content.length));
         }
 
+        if (msg.content.startsWith("age*") && msg.member?.id === process.env.OWNER_ID) {
+            let birthdays = await prisma.birthdays.findMany({ where: { guild_id: msg.guildId as string } });
+            let combined = 0;
+            const currentYear = new Date().getFullYear();
+            birthdays = birthdays.filter(x => x.birthday && x.birthday.getFullYear() > currentYear - 40);
+            birthdays = birthdays.sort((x, y) => Number(y.birthday) - Number(x.birthday));
+            birthdays.forEach(x => combined += yearsAgo(x.birthday as Date));
+            const average = Math.round(combined / birthdays.length);
+            msg.reply(`Average: ${average}\nMedian: ${yearsAgo(birthdays[Math.round(birthdays.length / 2)].birthday as Date)}\nRange: ${yearsAgo(birthdays[0].birthday as Date)} - ${yearsAgo(birthdays[birthdays.length - 1].birthday as Date)}`);
+            return;
+        }
+
+        if (msg.content === "levels*" && (msg.member?.id === process.env.OWNER_ID || msg.member?.id === "308274118183223297")) {
+            const staff = [
+                "325734188101926912",
+                "313259996739534849",
+                "436272901700976659",
+                "208623028798619648",
+                "750593174593994843",
+                "934705041879760926",
+                "316004787378192384",
+                "308274118183223297",
+                "217742553632473089",
+                "526876208521150503",
+                "174689636310974464",
+                "231932258519482368",
+                "500022065009786900",
+                "444610964575354901",
+                "568441584639541259",
+            ];
+
+            const staffLevels = await client.db.level.findMany({ where: { user_id: { in: staff }, guild_id: "396330910162616321" }, orderBy: { experience: "desc" } });
+
+            let response = "levels: \n";
+
+            for (const x of staffLevels) {
+                const member = await msg.guild?.members.fetch(x.user_id);
+                if (!member) continue;
+                const level = levelService.calculateLevel(x.experience);
+                response += `${member?.user.username} - ${level.level} - ${((level.currentXP / level.levelXP) * 100).toFixed(2)}%\n`;
+            }
+
+            await msg.reply(response);
+        }
+
         if (msg.content === "selfroles*" && msg.member?.id === process.env.OWNER_ID) {
             const main = await client.db.self_role_main.findFirst({ where: { guild_id: msg.guildId as string } }) as self_role_main;
             const roles = await client.db.self_role_roles.findMany({ where: { main_uuid: main.uuid } });
@@ -62,11 +116,11 @@ export default class InteractionCreate implements RavenEvent {
             const buttons: MessageActionRowComponent[] = [];
 
             roles.forEach((x) => {
-                embed.addField(`${x.emote} ${x.name}`, x.description);
+                embed.addField(`${x.emote} ${x.name} `, x.description);
                 buttons.push(
                     new MessageButton()
-                        .setCustomId(`selfrole_${x.uuid}`)
-                        .setLabel(`${x.emote} ${x.name}`)
+                        .setCustomId(`selfrole_${x.uuid} `)
+                        .setLabel(`${x.emote} ${x.name} `)
                         .setStyle("PRIMARY"),
                 );
             });
@@ -75,6 +129,20 @@ export default class InteractionCreate implements RavenEvent {
                 .addComponents(buttons);
 
             channel.send({ components: [component], embeds: [embed] });
+        }
+
+        // peepee poopoo
+        const channels: string[] = ["456103889733681182", "404032420963418142", "426343728991698945"];
+
+        if (channels.indexOf(msg.channelId) !== -1) {
+            const channel = msg.channel as GuildChannel;
+            console.log(`${msg.guild?.name} - ${channel.name} - ${msg.author.username}: `.magenta + msg.content.yellow || "EMPTY".red);
+
+            if (msg.attachments) {
+                msg.attachments.forEach(x => {
+                    console.log(`   ${msg.guild?.name} - ${channel.name} - ${msg.author.username}:> `.magenta + x.url.yellow || "EMPTY".red);
+                });
+            }
         }
     }
 }
