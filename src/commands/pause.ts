8cbb74adad9cfdfc03a1842f93df00c720901b63
valivia <valivia@xayania.com>
2022-05-31 15:24:20 +0000

    pause command

---
+++ src/commands/pause.ts
@@ -0,0 +1,21 @@
+import bot from "../app";
+
+export default async function pause(message: {
+    data: { guildId: string };
+}): Promise<{}> {
+    const { guildId } = message.data;
+
+    const client = bot.getClient();
+    const player = client.player;
+    const guild = await client.guilds.fetch(guildId);
+
+    const queue = player.getQueue(guild);
+
+    if (!queue || !queue.playing) throw "No music is playing";
+
+    const paused = !queue.connection.paused;
+
+    queue.setPaused(paused);
+
+    return { paused };
+}
