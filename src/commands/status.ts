9c1eb960bfa62e51ae71b52b3c77fe2d33648930
valivia <valivia@xayania.com>
2022-05-31 15:23:57 +0000

    status command
    Co-authored-by: Lukas Sabatschus <luksablp@gmail.com>

---
+++ src/commands/status.ts
@@ -0,0 +1,41 @@
+import bot from "../app";
+
+export default async function getStatus(): Promise<{}> {
+    const client = bot.getClient();
+
+    const id = client.user?.id;
+
+    if (!id)
+        return {
+            error: "Bot is not ready",
+        };
+
+    const guilds: Guild[] = [];
+
+    for (const guild of client.guilds.cache.values()) {
+        const item: Guild = { id: guild.id };
+        const channelId = guild.me?.voice.channelId;
+        channelId ? (item.channelId = channelId) : null;
+        guilds.push(item);
+    }
+
+    const status: Status = {
+        id,
+        uptime: process.uptime(),
+        guilds,
+    };
+
+    console.log(status.guilds);
+    return { ...status };
+}
+
+interface Status {
+    id: string;
+    uptime: number;
+    guilds: Guild[];
+}
+
+interface Guild {
+    id: string;
+    channelId?: string;
+}
