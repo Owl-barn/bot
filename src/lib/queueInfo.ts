8a20c749d1f8a5a2c5894bdb1387fc01d3b47c5f
valivia <valivia@xayania.com>
2022-06-18 21:27:57 +0000

    improved readability

--- src/lib/queueInfo.ts
+++ src/lib/queueInfo.ts
@@ -4,7 +4,10 @@ import RepeatMode from "../types/repeatmode";
 
 export default function queueInfo(queue: Queue) {
     const tracks = queue.getTracks();
-    let length = tracks.reduce((x, y) => x + y.durationMs, 0);
+    let length = tracks
+        .map((track) => track.durationMs)
+        .reduce((total, current) => total + current, 0);
+
     const current = queue.nowPlaying();
 
     if (current) length += current.durationMs - current.progressMs;
