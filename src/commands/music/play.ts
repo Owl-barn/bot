51655c7f86f6e74e2092d40e79e953f14731c1c8
valivia <valivia@xayania.com>
2022-09-23 18:47:52 +0000

    increased throttle on play because of reduce usage

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -49,7 +49,7 @@ module.exports = class extends Command {
 
             throttling: {
                 duration: 30,
-                usages: 3,
+                usages: 6,
             },
         });
     }
