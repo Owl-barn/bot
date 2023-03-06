c2e078e428891b64a0d42f53a3e3873739069e2a
valivia <valivia@xayania.com>
2023-03-06 16:42:19 +0000

    removed web module

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,5 @@ import env from "./modules/env";
 
 colors.enable();
 
-import GuildsController from "./routes/guilds/guilds.controller";
-import LeaderboardController from "./routes/leaderboard/leaderboard.controller";
-import OauthController from "./routes/oauth/oauth.controller";
-import StatusController from "./routes/status/controller";
-import WebServer from "./web";
-
 env;
-
-const web = new WebServer([
-    new OauthController(),
-    new GuildsController(),
-    new LeaderboardController(),
-    new StatusController(),
-]);
-web.listen();
-
 bot;
