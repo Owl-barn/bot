710dc47ef32d19c36e69ac1445c67a6ec8b175cd
valivia <valivia@xayania.com>
2023-03-11 17:14:31 +0000

    removed old file structure

--- src/modules/vc/commands/voice_config/index.ts
+++
@@ -1,9 +0,0 @@
-import { ParentCommand } from "@structs/command/parent";
-import { CommandGroup } from "@structs/command";
-
-export default ParentCommand({
-  name: "voiceconfig",
-  description: "Configure the private rooms.",
-  group: CommandGroup.config,
-});
-
