7a19f0014332ce70f7c092e2fbcc27769ad58c5e
Valivia <valivia@xayania.com>
2023-05-26 16:48:52 +0000

    🚀 Improved readme and env

--- src/lib/loaders/loadEnvironment .ts
+++ src/lib/loaders/loadEnvironment .ts
@@ -7,24 +7,25 @@ const env = cleanEnv(process.env, {
     choices: ["development", "production", "staging"],
     default: "production",
   }),
-
-  // Main
-  PASSWORD: str(),
-  ADDRESS: url(),
   LOG_LEVEL: str({
     choices: ["error", "warn", "info", "verbose", "debug", "silly"],
     default: "info",
   }),
 
+  // Server
+  PASSWORD: str({ default: "password" }),
+  ADDRESS: url({ default: "ws://bot:3001" }),
+
   // Music
   IDLE_TIMEOUT: num({
-    default: 120000, // 2 minutes
+    default: 300000, // 5 minutes
+    desc: "How long to wait before disconnecting from voice channel (miliseconds)",
   }),
 
   // Spotify
   SP_ID: str(),
   SP_SECRET: str(),
-  SP_RT: str(),
+  SP_RT: str({ desc: "Spotify refresh token" }),
   SP_MARKET: str(),
 
 });
