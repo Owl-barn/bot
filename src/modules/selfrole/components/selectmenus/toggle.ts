13267fdd39b8550baf49e954e5c01235fe87d6f8
valivia <26413018+valivia@users.noreply.github.com>
2023-12-19 23:16:03 +0000

    ⚒️ Improved SelfRole menu usability

--- src/modules/selfrole/components/selectmenus/toggle.ts
+++ src/modules/selfrole/components/selectmenus/toggle.ts
@@ -15,6 +15,9 @@ export default SelectMenu(
     const roleId = msg.values[0].trim();
 
     if (!user) return error;
-    return await toggleRole(user, roleId);
+    const result = await toggleRole(user, roleId);
+    // update the select menu with an exact copy of itself, to remove the user's selection (i hate this being a thing)
+    await msg.update({ components: msg.message.components });
+    return result;
   },
 );
