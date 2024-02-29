2c76104b48361456cc54d9cf5d49befc1c5f03eb
Martin Löffler <me@fatalerrorcoded.eu>
2024-02-29 16:18:32 +0100

    ✨ Send confession as a regular message to get native content embeds
    why hasn't anyone thought of this yet?

--- bot/src/modules/core/commands/confess.ts
+++ bot/src/modules/core/commands/confess.ts
@@ -39,16 +39,7 @@ export default Command(
     if (!confessionChannel || !confessionChannel.isTextBased())
       return { content: `Couldn't find a channel with the name \`${channelName}\``, ephemeral: true };
 
-    const confessionEmbed = embedTemplate()
-      .setTitle(`Confession`)
-      .setDescription(confession);
-
-    const imageUrl = await getImageUrl(confession);
-    if (imageUrl) {
-      confessionEmbed.setImage(imageUrl);
-    }
-
-    const sent = await confessionChannel.send({ embeds: [confessionEmbed] })
+    const sent = await confessionChannel.send(`**Confession:** ${confession}`)
       .catch(() => false);
 
     if (!sent) throw "couldn't send confession";
@@ -59,42 +50,3 @@ export default Command(
     return { embeds: [responseEmbed], ephemeral: true };
   }
 );
-
-const tenorRegex = /\/?.*\/view\/.*-[0-9]+/;
-const getImageUrl = async (text: string): Promise<string | undefined> => {
-  const imageExtensions = ["jpg", "jpeg", "png", "apng", "webp", "gif", "avif"];
-  try {
-    let url = new URL(text);
-    if (url.protocol !== "http:" && url.protocol !== "https:")
-      return undefined;
-
-    let urlPath = path.parse(url.pathname);
-    if (url.host === "tenor.com" && url.pathname.match(tenorRegex)) {
-      // find out gif name from URL
-      // make a GET request to the URL
-      const response = await axios.get(url.href);
-      const data = await response.data;
-      // find the GIF URL
-      // remove the "-gif-23456789" part
-      let gifName = urlPath.base;
-      while (gifName !== "") {
-        gifName = gifName.split("-").slice(0, -1).join("-");
-        // find the GIF URL
-        let regex = new RegExp(`https:\\/\\/media[0-9]?.tenor.com(\/m)?\\/.{16}\\/${gifName}\\.gif`, "g");
-        let match = data.match(regex);
-        if (match) {
-          return match[0];
-        }
-      }
-    }
-
-    let extension = urlPath.ext.substring(1); // substring removes the leading dot from the extension
-    if (imageExtensions.indexOf(extension) != -1) {
-      return text;
-    }
-  } catch {
-    return undefined;
-  }
-
-  return undefined;
-}
