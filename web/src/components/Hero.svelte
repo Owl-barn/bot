b15e315502b234fa2df7522e6ad96e221b4f78c0
valivia <26413018+valivia@users.noreply.github.com>
2025-03-17 16:06:28 +0000

    ✨ Animated membercount

--- web/src/components/Hero.svelte
+++ web/src/components/Hero.svelte
@@ -13,11 +13,27 @@
     <Owl />
     <h1>Hootsifer</h1>
     <p>A multi-purpose Discord bot</p>
-    <p>Trusted by <var>{stats.memberCount}</var> members</p>
-    <p>In <var>{stats.guildCount}</var> communities</p>
+    <p>
+        Trusted by <var
+            style="--animatedNumber: {stats.memberCount}; --duration: 4s"
+            aria-label={String(stats.memberCount)}
+        ></var> members
+    </p>
+    <p>
+        In <var
+            style="--animatedNumber: {stats.guildCount}; --duration: 1s"
+            aria-label={String(stats.guildCount)}
+        ></var> communities
+    </p>
 </header>
 
 <style lang="scss">
+    @property --num {
+        syntax: "<integer>";
+        initial-value: 0;
+        inherits: false;
+    }
+
     header {
         text-align: center;
 
@@ -36,6 +52,23 @@
             color: var(--theme-accent);
             font-style: normal;
             font-weight: 600;
+
+            animation: counter var(--duration) forwards alternate ease-out;
+            counter-reset: num var(--num);
+
+            &::before {
+                content: var(--num);
+                content: counter(num);
+            }
+        }
+
+        @keyframes counter {
+            from {
+                --num: 0;
+            }
+            to {
+                --num: var(--animatedNumber);
+            }
         }
     }
 </style>
