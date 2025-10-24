c2b33b3c6a51ad3be00f54ff4c9005ec59bc34ad
Martin Löffler <me@fatalerrorcoded.eu>
2025-10-25 01:31:07 +0200

    🩹 Hacky workaround to get playback on unpatched versions of yt-dlp

--- owlet/src/lib/streamers/youtube.ts
+++ owlet/src/lib/streamers/youtube.ts
@@ -2,6 +2,7 @@ import { PassThrough, Readable } from "stream";
 import { Track } from "discord-player";
 import { YoutubeiExtractor } from "discord-player-youtubei";
 import { YtDlp } from "ytdlp-nodejs";
+import { spawn } from "child_process";
 
 interface YoutubeStreamerOptions {
   // How much space to allocate for the download buffer, by default 32 MB
@@ -18,8 +19,8 @@ export const buildYoutubeStreamer = async (options?: YoutubeStreamerOptions) => 
   }
 
   const highWaterMark = options?.highWaterMark ?? (32 * 1024 * 1024);
-  const ytdlpFormat = options?.ytdlpFormat ?? "251/bestaudio";
-  const createStream = async (track: Track, _extractor: YoutubeiExtractor): Promise<Readable> => {
+  // const ytdlpFormat = options?.ytdlpFormat ?? "251/bestaudio";
+  /* const createStream = async (track: Track, _extractor: YoutubeiExtractor): Promise<Readable> => {
     const stream = ytdlp.stream(track.url, {
       format: ytdlpFormat,
     });
@@ -27,7 +28,27 @@ export const buildYoutubeStreamer = async (options?: YoutubeStreamerOptions) => 
     const passthrough = new PassThrough({ highWaterMark });
     stream.pipe(passthrough);
     return passthrough;
+  }; */
+
+  // HACK: workaround to get around current Youtube limitations, at least until yt-dlp updates
+  const createStreamWorkaround = async (track: Track, _extractor: YoutubeiExtractor): Promise<Readable> => {
+    const stream = ytdlp.stream(track.url, {
+      format: "91",
+    });
+
+    const ffmpeg = spawn("ffmpeg", [
+      "-i", "pipe:0",
+      "-vn",
+      "-acodec", "copy",
+      "-f", "adts",
+      "pipe:1",
+    ]);
+
+    const passthrough = new PassThrough({ highWaterMark });
+    stream.pipe(ffmpeg.stdin);
+    ffmpeg.stdout.pipe(passthrough);
+    return passthrough;
   };
 
-  return createStream;
+  return createStreamWorkaround;
 };
